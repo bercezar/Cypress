509227812331ee7f9c1adfb68723f90d54d42150
Bernardo Cezar <b.cezar1812@gmail.com>
2026-05-28 17:50:42 -0300

    test: implement remove products from cart

---
+++ cypress/e2e/remove_product_from_cart.cy.js
@@ -0,0 +1,32 @@
+describe("remove_product_from_cart", () => {
+  beforeEach(() => {
+    cy.createUserDynamic();
+  });
+  it("Remove Products From Cart", () => {
+    const targetIds = ["1", "4"];
+    targetIds.forEach((id) => {
+      cy.get(`[data-product-id="${id}"]`).first().click({ force: true });
+      cy.contains("Continue Shopping").should("be.visible").click();
+    });
+    cy.contains(" Cart").click();
+
+    cy.contains("Shopping Cart").should("be.visible");
+
+    targetIds.forEach((id) => {
+      cy.get(`#product-${id}`).within(() => {
+        cy.get(".cart_quantity_delete").should("be.visible").click();
+      });
+    });
+
+    // ASSERT
+    cy.get("#empty_cart").within(() => {
+      cy.get(".text-center b").should("have.text", "Cart is empty!");
+    });
+
+    // CLEANUP
+    cy.contains(" Delete Account").click();
+
+    cy.contains("Account Deleted!").should("be.visible");
+    cy.get('[data-qa="continue-button"]').click();
+  });
+});
